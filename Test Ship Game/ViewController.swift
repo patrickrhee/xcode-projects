3f46b77968d958dc63b28e41e39fb8ff38353d33
Padraig Lee <smtmaild@gmail.com>
2019-12-25 22:54:19 -1000

    Initial Commit

---
+++ Test Ship Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Ship Game
+//
+//  Created by Patrick Rhee on 12/25/19.
+//  Copyright © 2019 Rhee Family. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
